ff913a301fc07f5e28f8bc0b9d46419b26ba0457
Robin Ding <ruobingding@gmail.com>
2017-10-24 22:16:12 +0800

    add init state

--- lesson109/src/index.jsx
+++ lesson109/src/index.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import '../res/style/main.css';
-
 import { Provider } from 'react-redux';
 import { TodoApp } from './components/TodoApp';
+import { todos, doers } from '../constants/MockData';
 
-const store = configureStore()
+const initState = {
+  todos,
+  doers,
+  doer: null,
+  errMsg: null,
+};
+
+const store = configureStore(initState)
 
 ReactDOM.render(
   <Provider store={store}>
