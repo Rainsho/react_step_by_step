f734c43fa9e241469bec2ac37ca56a83dd97419a
Robin <ruobingding@gmail.com>
2017-10-31 16:36:05 +0800

    Update index.js

--- lesson109/simpleDemo/index.js
+++ lesson109/simpleDemo/index.js
@@ -1,17 +1,57 @@
 const createStore = require('redux').createStore;
+const applyMiddleware = require('redux').applyMiddleware;
+const combineReducers = require('redux').combineReducers;
+const createLogger = require('redux-logger').createLogger;
 
-function counter(state = 0, action){
+const initState = {
+  counter:0
+}
+
+/*
+function counter(state = initState, action){
   switch(action.type){
     case 'INCREMENT':
-      return state + 1
+      let counter = state.counter + 1;
+      let temp = {counter : counter}
+      return Object.assign({},state,temp)
     case 'DECREMENT':
-      return state - 1
+      let counter1 = state.counter - 1;
+      let temp1 = {counter : counter1}
+      return Object.assign({},state,temp1)
+    default:
+      return state
+  }
+}
+*/
+
+
+function counterAdd(state = initState,action){
+  switch(action.type){
+    case 'INCREMENT':
+      let counter = state.counter + 1;
+      let temp = {counter : counter}
+      return Object.assign({},state,temp)
     default:
       return state
   }
 }
 
-let store = createStore(counter);
+function counterSub(state = initState,action){
+  switch(action.type){
+    case 'DECREMENT':
+      let counter1 = state.counter - 1;
+      let temp1 = {counter : counter1}
+      return Object.assign({},state,temp1)
+    default:
+      return state
+  }
+}
+
+const counter = combineReducers({counterAdd,counterSub})
+
+const logger = createLogger();
+let store = createStore(counter,
+applyMiddleware(logger));
 
 store.subscribe(() => console.log(store.getState()));
 
