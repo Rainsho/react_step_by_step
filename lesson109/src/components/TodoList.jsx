e4615725777a57952ca093bcdf269363820ae5ab
Robin <ruobingding@gmail.com>
2017-11-01 16:35:47 +0800

    Update TodoList.jsx

--- lesson109/src/components/TodoList.jsx
+++ lesson109/src/components/TodoList.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import TodoView from './TodoView';
 
-function TodoList({ todos }) {
+function TodoList({todos, deleteTodo, mark}) {
   if (todos.length === 0) return null;
-  const views = todos.map(todo => <TodoView key={todo.tid} todo={todo} />);
+  const views = todos.map(todo => <TodoView key={todo.tid} todo={todo} deleteTodo={deleteTodo} mark= {mark}/>);
   return (
     <div>
       <div className="todo-title">
