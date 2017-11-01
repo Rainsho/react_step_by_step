9e3d4c990821cfbb4cdd619971869d099901640c
Robin <ruobingding@gmail.com>
2017-11-01 16:31:13 +0800

    Update index.jsx

--- lesson109/src/index.jsx
+++ lesson109/src/index.jsx
@@ -2,17 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import '../res/style/main.css';
 import { Provider } from 'react-redux';
-import { TodoApp } from './components/TodoApp';
-import { todos, doers } from '../constants/MockData';
-
-const initState = {
-  todos,
-  doers,
-  doer: null,
-  errMsg: null,
-};
-
-const store = configureStore(initState)
+import TodoApp from './containers/App';
+import configureStore from './store/configureStore';
+const store = configureStore();
 
 ReactDOM.render(
   <Provider store={store}>
