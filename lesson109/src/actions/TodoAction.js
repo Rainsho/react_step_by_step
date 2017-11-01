2ffa19fc21f3679dad6ae14abe3151a98455d09a
Robin <ruobingding@gmail.com>
2017-11-01 16:32:54 +0800

    Update TodoAction.js

--- lesson109/src/actions/TodoAction.js
+++ lesson109/src/actions/TodoAction.js
@@ -1,12 +1,58 @@
-import { LOGIN, LOGOUT, ADD_TODO, MARK_TODO, DELETE_TODO } from '../constants/ActionTypes';
+import { LOGIN, LOGOUT, ADD_TODO, MARK_TODO, DELETE_TODO, REGISTER} from '../constants/ActionTypes';
 
-export function login(name,pswd){
-  return {
-      type: LOGIN,
-      payload: {
-        name, pswd,
-      },
+
+
+ export function createLoginAction(name,pswd){
+      return {
+          type: LOGIN,
+          payload: {
+            name, pswd
+          }
+      };
     }
-}
 
-export const logout = { type: LOGOUT }
+ export function createRegisterAction(name,pswd){
+      return {
+          type: REGISTER,
+          payload: {
+            name, pswd
+          }
+      };
+    }
+
+export function createLogoutAction(){
+      return {
+        type:LOGOUT
+      }
+    }
+
+export function createAddTodoAction(content){
+      return {
+          type: ADD_TODO,
+          payload: {
+            content
+          }
+        };
+    }
+
+ export function createMarkTodoAction(tid){
+      return {
+          type: MARK_TODO,
+          payload: {
+            tid
+          }
+        };
+    }
+
+ export function createDeleteTodoAction(tid){
+       return {
+          type: DELETE_TODO,
+          payload: {
+            tid
+          }
+        };
+     }
+
+
+
+
