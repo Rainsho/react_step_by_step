2a983f3d11119bd63c987197b246d9e3bad553ce
Rainsho <djkyux@163.com>
2018-06-24 00:40:02 +0800

    write test

--- xdemo104/sagas.js
+++ xdemo104/sagas.js
@@ -1,12 +1,12 @@
 import { delay } from 'redux-saga';
-import { put, takeEvery, all } from 'redux-saga/effects';
+import { put, takeEvery, all, call } from 'redux-saga/effects';
 
 export function* helloSaga() {
   console.log('Hello Sagas!');
 }
 
 export function* incrementAsync() {
-  yield delay(1000);
+  yield call(delay, 1000);
   yield put({ type: 'INCREMENT' });
 }
 
