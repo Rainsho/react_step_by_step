fba455fa541d7a6209e1f143951f5a4b9d52272b
Rainsho <djkyux@163.com>
2018-06-24 00:12:59 +0800

    init redux-saga-beginner-tutorial

---
+++ xdemo104/Counter.js
@@ -0,0 +1,25 @@
+/*eslint-disable no-unused-vars */
+import React, { Component, PropTypes } from 'react'
+
+const Counter = ({ value, onIncrement, onDecrement }) =>
+      <div>
+        <button onClick={onIncrement}>
+          Increment
+        </button>
+        {' '}
+        <button onClick={onDecrement}>
+          Decrement
+        </button>
+        <hr />
+        <div>
+          Clicked: {value} times
+        </div>
+      </div>
+
+Counter.propTypes = {
+  value: PropTypes.number.isRequired,
+  onIncrement: PropTypes.func.isRequired,
+  onDecrement: PropTypes.func.isRequired
+}
+
+export default Counter
